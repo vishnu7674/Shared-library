ccaf1aff3c87b4534899e4f0aa9945e4f7bd28a7
vishnu7674 <91712055+vishnu7674@users.noreply.github.com>
2024-08-17 16:23:35 +0530

    Update firstpipeline.groovy

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -13,15 +13,14 @@ def call(Map pipelineparams){
             APP_Name = "${pipelineparams.appName}" //this value should come from the microservices
         }
         stages {
-        stage ('addition') {
-            steps {
-                echo "priniting sum of 2 numbers" 
-                println calculator.add(3,4)
-                echo "**********microservice name is: ${APP_Name} ******"
+            stage ('addition') {
+                steps {
+                    echo "priniting sum of 2 numbers" 
+                    println calculator.add(3,4)
+                    echo "**********microservice name is: ${APP_Name} ******"
+                }
             }
-        }
         }
     }
 }
-    
   
