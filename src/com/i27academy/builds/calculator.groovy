a768ad2574fa3d14fd4e00e8fd4af2321db7da7c
vishnu7674 <91712055+vishnu7674@users.noreply.github.com>
2024-08-17 15:15:02 +0530

    deleted

--- src/com/i27academy/builds/calculator.groovy
+++ src/com/i27academy/builds/calculator.groovy
@@ -1,6 +1,21 @@
 package com.i27academy.builds;
 
-//all the methods
+// all the methods
 class calculator {
-    
+    def jenkins
+    calculator(jenkins) {
+        this.jenkins = jenkins
+    }
+}
+
+
+// additon method
+def add(firstnumber, secondnumber) {
+    // logical code
+    return firstnumber+secondnumber
+}
+
+// multiply
+def multiply((firstnumber, secondnumber)) {
+    return firstnumber * secondnumber
 }
