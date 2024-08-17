e5fef65ee88938188bfb865a81072e54ace77520
vishnu7674 <91712055+vishnu7674@users.noreply.github.com>
2024-08-17 16:16:25 +0530

    Update firstpipeline.groovy

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -5,21 +5,23 @@ import com.i27academy.bUilds.calculator
 def call(Map pipelineparams){
     // An instance of the class called calculator is created
 
-calculator calculator = new calculator(this)
+    calculator calculator = new calculator(this)
 
-pipeline {
-    agent any
-    environment {
-        APP_Name = ${pipelineparams.appName} //this value should come from the microservices
-    }
-    stages {
+    pipeline {
+        agent any
+        environment {
+            APP_Name = ${pipelineparams.appName} //this value should come from the microservices
+        }
+        stages {
         stage ('addition') {
             steps {
-                echo "priniting sum of 2 numbers"
+                echo "priniting sum of 2 numbers" 
                 println calculator.add(3,4)
                 echo "**********microservice name is: ${APP_Name} ******"
             }
         }
+        }
     }
 }
-}
+    
+  
